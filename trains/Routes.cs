35848e51fb9581d7376007a7df9b1af790c7cd89
tasimba.chirindo <tasimba.chirindo@opsi.co.za>
2018-05-16 13:22:20 +0200

    Added methods for calculating shortest routes and number of routes

--- trains/Routes.cs
+++ trains/Routes.cs
@@ -64,5 +64,89 @@ namespace trains
       return distance;
     }
 
+    public int findNumberOfRoutes(Node origin, Node destination, int depth, int maxStops)
+    {
+      int routes = 0;
+      // Sanity check to verify that origin and destination nodes exist in the route table
+      if (this.routeTable.ContainsKey(origin) && this.routeTable.ContainsKey(destination))
+      {
+        depth++;
+        if (depth > maxStops)
+        {
+          return 0;
+        }
+        origin.visited = true;
+        Edge edge;
+        bool hasValue = this.routeTable.TryGetValue(origin, out Edge value);
+        if (hasValue)
+        {
+          edge = value;
+          while (edge != null)
+          {
+            if (edge.destination.Equals(destination))
+            {
+              routes++;
+              edge = edge.next;
+              continue;
+            }
+            else if (!edge.destination.visited)
+            {
+              routes += findNumberOfRoutes(edge.destination, destination, depth, maxStops);
+            }
+            edge = edge.next;
+          }
+        }
+      }
+      else
+      {
+        throw new Exception("ROUTE DOESNT EXIST");
+      }
+      origin.visited = false;
+      return routes;
+    }
+
+    public int findShortestRoute(Node origin, Node destination, int weight, int shortestRoute)
+    {
+      // Sanity check to verify that origin and destination nodes exist in the route table
+      if (this.routeTable.ContainsKey(origin) && this.routeTable.ContainsKey(destination))
+      {
+        origin.visited = true;
+        Edge edge;
+        bool hasValue = this.routeTable.TryGetValue(origin, out Edge value);
+        if (hasValue)
+        {
+          edge = value;
+          while (edge != null)
+          {
+            if (edge.destination == destination || !edge.destination.visited)
+            {
+              weight += edge.weight;
+            }
+
+            if (edge.destination.Equals(destination))
+            {
+              if (shortestRoute == 0 || weight < shortestRoute)
+              {
+                shortestRoute = weight;
+              }
+              origin.visited = false;
+              return shortestRoute; 
+            }
+            else if (!edge.destination.visited)
+            {
+              shortestRoute = findShortestRoute(edge.destination, destination, weight, shortestRoute);
+              weight -= edge.weight;
+            }
+            edge = edge.next;
+          }
+        }
+      }
+      else
+      {
+        throw new Exception("ROUTE DOESNT EXIST");
+      }
+      origin.visited = false;
+      return shortestRoute;
+    }
   }
 }
