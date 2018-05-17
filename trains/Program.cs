72a3b387aea882dce7a3e30abca75ea075dc1efb
tasimba.chirindo <tasimba.chirindo@opsi.co.za>
2018-05-17 08:54:09 +0200

    Supply node and edge data using json text files located in file system. Added wrapper methods to parse this data

--- trains/Program.cs
+++ trains/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,57 @@ namespace trains
   {
     static void Main(string[] args)
     {
+      List<String> nodes = readTextFile("nodes.json");
+      List<String> edges = readTextFile("edges.json");
+    }
+
+    // reads text file from JSON file from file system.
+    public static List<String> readTextFile(String fileName)
+    {
+      string reader = File.ReadAllText(@".\" + fileName);
+      return JsonConvert.DeserializeObject<List<string>>(reader);
+    }
+
+    // generate a list of edges from user input of edges supplied in input text file
+    public static List<Edge> generateEdgesFromJsonInput(List<string> input)
+    {
+      if (input == null || !input.Any())
+      {
+        return null;
+      }
+      else
+      {
+        List<Edge> edges = new List<Edge>();
+        foreach (var e in input)
+        {
+          var o = new Node(e[0].ToString());
+          var d = new Node(e[1].ToString());
+          var w = int.Parse(e.Substring(2));
+
+          Edge edge = new Edge(o, d, w);
+          edges.Add(edge);
+        }
+        return edges;
+      }
+    }
+
+    // generate a list of nodes from user input of nodes supplied in input text file
+    public static List<Node> generateNodesFromJsonInput(List<string> input)
+    {
+      if (input == null || !input.Any())
+      {
+        return null;
+      }
+      else
+      {
+        List<Node> nodes = new List<Node>();
+        foreach (var n in input)
+        {
+          var node = new Node(n[0].ToString());
+          nodes.Add(node);
+        }
+        return nodes;
+      }
     }
   }
 }
